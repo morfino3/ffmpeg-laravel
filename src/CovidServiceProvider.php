77a550f096f9c99ef12cf128995b654bad7b96a1
Mike Ambait <mkambait@revlv.net>
2020-02-27 14:29:49 +0800

    fixed serviceProvider

--- src/CovidServiceProvider.php
+++ src/CovidServiceProvider.php
@@ -18,9 +18,9 @@ class CovidServiceProvider extends IlluminateServiceProvider
             __DIR__ . '/../config/laboratory.covid.php' => config_path('laboratory.bucket.php'),
         ], 'laboratory-covid:config');
 
-        // $this->publishes([
-        //      __DIR__ . '/../migrations/' => database_path('/migrations'),
-        //  ], 'laboratory-bucket:migrations');
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/laboratory.covid.php', 'laboratory.covid'
+        );
     }
 
     /**
@@ -39,9 +39,9 @@ class CovidServiceProvider extends IlluminateServiceProvider
         //     return $model;
         // });
 
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/laboratory.covid.php', 'laboratory.covid'
-        );
+        // $this->mergeConfigFrom(
+        //     __DIR__ . '/../config/laboratory.covid.php', 'laboratory.covid'
+        // );
 
         $this->app->singleton('covid', function ($app) {
             return $app->make(FFMpeg::class);
